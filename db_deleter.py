67e800b66ccfcaff5ac32553b2edf68bcf8091aa
qone2 <whrbdnjs33@naver.com>
2021-09-07 21:55:22 +0900

    localhost주소로 인한 성능 하락 개선
    127.0.0.1 대신 localhost를 쓰면 호스트파일을 열람해야해서 시간이 걸린다.

--- db_deleter.py
+++ db_deleter.py
@@ -2,7 +2,7 @@ import requests
 import json
 from urllib.parse import quote_plus
 
-resp1 = requests.get("http://localhost:8000/matzip").json()
+resp1 = requests.get("http://127.0.0.1:8000/matzip").json()
 
 for re in resp1:
-    de = requests.delete("http://localhost:8000/matzip/" + str(re["id"]))
+    de = requests.delete("http://127.0.0.1:8000/matzip/" + str(re["id"]))
